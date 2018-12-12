69904dc1f91d877f47b12276fda7ca0887ac885c
Jolken <Jolken@outlook.com>
2018-12-12 22:52:58 +0200

    Login as user?

--- ProjectMartinB/Program.cs
+++ ProjectMartinB/Program.cs
@@ -10,6 +10,9 @@ using System.IO;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
+using System.Linq;
+using System.Net;
+using System.Collections.Specialized;
 /*   Invite link
 *   https://discordapp.com/oauth2/authorize?client_id=521057341563469844&scope=bot&permissions=36883520
 * 
@@ -74,7 +77,13 @@ namespace ProjectMartinB
             await ctx.RespondAsync($"МБ я, МБ! Уродина, {ctx.User.Mention}!");
         }
 
-        [RequireOwner]
+        [Command("exe")]
+        public async Task execute(CommandContext ctx, string command)
+        {
+            await ctx.RespondAsync(command);
+        }
+
+        
         [Command("join")]
         public async Task Join(CommandContext ctx)
         {
@@ -106,8 +115,7 @@ namespace ProjectMartinB
             vnc.Disconnect();
             await ctx.RespondAsync("👌");
         }
-
-        [RequireOwner]
+        
         [Command("play")]
         public async Task Play(CommandContext ctx, string playing)
         {
@@ -205,6 +213,33 @@ namespace ProjectMartinB
         static async Task MainAsync(string[] args)
         {
 
+            /*
+            var http = (HttpWebRequest)WebRequest.Create(new Uri("https://discordapp.com/api/v6/auth/login"));
+            http.Accept = "application/json";
+            http.ContentType = "application/json";
+            http.Method = "POST";
+            string parsedContent = Newtonsoft.Json.JsonSerializer.
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] bytes = encoding.GetBytes(parsedContent);
+            Stream newStream = http.GetRequestStream();
+            newStream.Write(bytes, 0, bytes.Length);
+            newStream.Close();
+            var response = http.GetResponse();
+
+            var stream = response.GetResponseStream();
+            var srr = new StreamReader(stream);
+            var content = srr.ReadToEnd();
+
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["email"] = "email";
+                data["password"] = "pass";
+                wb.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                var res = wb.UploadValues("https://discordapp.com/api/v6/auth/login", data);
+                response = Encoding.Default.GetString(res);
+            }
+            */
             var json = "";
             using (var fs = File.OpenRead("config.json"))
             using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
@@ -213,8 +248,10 @@ namespace ProjectMartinB
 
             discord = new DiscordClient(new DiscordConfiguration
             {
+                //Token = cfgjson.Token,
+                //TokenType = TokenType.Bot,
                 Token = cfgjson.Token,
-                TokenType = TokenType.Bot,
+                TokenType = TokenType.User,
                 UseInternalLogHandler = true,
                 LogLevel = LogLevel.Debug,
                 AutoReconnect = true,
